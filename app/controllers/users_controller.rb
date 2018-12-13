a9ceae8d196fc705c8a76ce355b48588d04e18d4
kento.horimoto <kentojason@yahoo.co.jp>
2018-12-13 11:42:42 +0000

    user login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in,only: [:index, :show]
+  
+  
+  
+  def index
+      @users = User.all.page(params[:page])
+  end
+
+  def show
+    @user= User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user =User.new(user_params)#プライベートなメソッド
+    
+    if @user.save
+      flash[:success] ='ユーザーを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] ='ユーザーの登録に失敗しました。'
+      render :new
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+#この password から暗号化され password_digest として保存されます。password_confirmation は password の確認のために使用されます。
+end
