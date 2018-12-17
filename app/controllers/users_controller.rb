db7941b072c2868263fc3f437993ef13cfb4dbcb
kento.horimoto <kentojason@yahoo.co.jp>
2018-12-17 12:54:51 +0000

    Favorite Function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in,only: [:index, :show]
-  
-  
+  before_action :require_user_logged_in,only: [:index, :show,:likes]
   
   def index
       @users = User.all.page(params[:page])
@@ -9,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user= User.find(params[:id])
+    @micropost =current_user.microposts.build
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
@@ -39,6 +38,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
     counts(@user)
+ end
+ 
+ def likes
+    @user = User.find(params[:id])
+    @microposts = @user.favorites.page(params[:page])
+     counts(@user)
  end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,16 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   
-  has_many :microposts
-  has_many :relationships
+  has_many :microposts 
+  has_many :relationships , dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship',foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  #お気に入り機能課題
+  has_many :likes , dependent: :destroy
+  has_many :favorites,through: :likes,source: :micropost
+  
   
   def follow(other_user)
     unless self == other_user
@@ -30,4 +35,17 @@ class User < ApplicationRecord
   def feed_microposts
       Micropost.where(user_id: self.following_ids+[self.id])
   end    
+  
+  def favorite(micropost)
+    self.likes.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    like = self.likes.find_by(micropost_id: micropost.id)
+    like.destroy if like
+  end  
+  
+  def favorite_post?(micropost)
+    self.favorites.include?(micropost)
+  end  
 end
